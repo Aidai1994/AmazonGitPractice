735f1bddf1b832ded7692e9e1c0373ae4fd4ca5d
Aidai <aidai.m94@gmail.com>
2022-02-21 21:08:15 +0600

    fix/BugFix T1320

---
+++ src/main/java/com/digital_nomads/javaFall2021/amazon/AmazonTest.java
@@ -0,0 +1,9 @@
+package com.digital_nomads.javaFall2021.amazon;
+
+public class AmazonTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Amazon Test");
+    }
+}
