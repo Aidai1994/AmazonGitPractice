f6e87bc65530cc2c5e136a65fbf5747edbc52620
Aidai <aidai.m94@gmail.com>
2022-02-22 20:07:42 +0600

    deleted sout's

--- src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
+++ src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
@@ -8,16 +8,15 @@ public class BankerDemo {
         DemirBank nursultanDemirBankAccount = new DemirBank(1234567891l,123456789);
         DemirBank urmatDemirBankAccount = new DemirBank(12548451289l,6959856);
 
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
         nursultanDemirBankAccount.deposit(5000);
-        System.out.println("After deposit: " + nursultanDemirBankAccount.getBalance());
+
 
        transferFunds(nursultanDemirBankAccount,urmatDemirBankAccount,2000);
 
-        System.out.println("Urmat's balance after transfer is: " + urmatDemirBankAccount.getBalance());
-        System.out.println("Nursultan's balance after trancfer is: " + nursultanDemirBankAccount.getBalance());
+
 
         transferFunds(urmatDemirBankAccount,nursultanDemirBankAccount,2101);
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
     }
 }
